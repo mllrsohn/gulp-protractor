bca473e685ab498bd74cb1ed09207b0102093c88
Steffen Müller <steffen@mllrsohn.com>
2014-04-29 08:44:15 +0200

    updated packages and exmaple #21

--- example/protractor.conf.js
+++ example/protractor.conf.js
@@ -2,9 +2,8 @@
 // https://raw.github.com/angular/protractor/master/example/conf.js
 exports.config = {
   // The address of a running selenium server.
-  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.39.0.jar',
+  seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar', // Make use you check the version in the folder
   //seleniumAddress: 'http://localhost:4444/wd/hub',
-
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
