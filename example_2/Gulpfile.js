181e0147cf2375dc5e785fdcde13951bdc0fcb53
Luke Horvat <lukehorvat@gmail.com>
2014-06-07 23:56:48 +1000

    Eliminated need for "app" variable in Example 2

--- example_2/Gulpfile.js
+++ example_2/Gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp = require('gulp');
 require('gulp-help')(gulp);
-var express = require('express');
 var gp = require('gulp-protractor');
 var args = require('yargs').argv;
-var server, isCI = args.type === 'ci';
+var express = require('express');
+var http = require('http');
+var server = http.createServer(express().use(express.static(__dirname + '/build/')));
+var isCI = args.type === 'ci';
 
 // Download and update the selenium driver
 gulp.task('e2etests:webdriver_manager_update', 'updates the selenium server standalone jar file ', gp.webdriver_update);
@@ -15,6 +17,7 @@ gulp.task('e2etests:run', 'runs e2etests using protractor.conf', ['e2etests:serv
 		.pipe(gp.protractor({
 			configFile: './protractor.conf.js'
 		})).on('error', function(e) {
+			server.close();
 			if(isCI) {
 				throw e;
 			} else {
@@ -22,18 +25,11 @@ gulp.task('e2etests:run', 'runs e2etests using protractor.conf', ['e2etests:serv
 			}
 			cb();
 		}).on('end', function() {
-			// Close the Server
-			if (server) {
-				server.close();
-			}
-			// Gulp Callback
+			server.close();
 			cb();
-
 		});
 });
 
 gulp.task('e2etests:server', 'starts a development webserver', function(cb) {
-	var app = express();
-	app.use(express.static(__dirname + '/build/'));
-	server = app.listen(9001, cb);
+	server.listen(9001, cb);
 });
